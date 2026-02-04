35b7324a463598800007a835aaf7804627646d2e
Clogxzyamane <131625473+Clogxzyamane@users.noreply.github.com>
2026-02-04 19:08:30 +0000

    Progress on the Mechanics
    added  Movement  (work in progress)*
    and camara character follow

---
+++ ProjectFurLure/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float horizontalInput;
+    float moveSpeed =  8f;
+    bool isFacingRight = false;
+    float jumpPower = 5f;
+    bool isJumping = false;
+
+    Rigidbody2D rb;
+
+    void Start()
+
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        FlipSprite();
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isJumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+            {
+                isJumping = false;
+            }
+        
+    
+}
+
