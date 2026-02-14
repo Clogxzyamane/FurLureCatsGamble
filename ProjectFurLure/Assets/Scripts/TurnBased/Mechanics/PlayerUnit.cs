de4538e16517c85f093374909d1215f8165de004
Clogxzyamane <131625473+Clogxzyamane@users.noreply.github.com>
2026-02-14 18:40:31 +0000

    Progress

--- ProjectFurLure/Assets/Scripts/TurnBased/Mechanics/PlayerUnit.cs
+++ ProjectFurLure/Assets/Scripts/TurnBased/Mechanics/PlayerUnit.cs
@@ -5,6 +5,28 @@ using UnityEngine;
 public class PlayerUnit : MonoBehaviour
 {
 
-    [SerializeField] int MaxHealth;
-    [SerializeField] int currentHealth;
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+    public bool TakeDamage(int dmg)
+    {
+        currentHP -= dmg;
+
+        if (currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHP += amount;
+        if (currentHP > maxHP)
+            currentHP = maxHP;
+    }
 }
